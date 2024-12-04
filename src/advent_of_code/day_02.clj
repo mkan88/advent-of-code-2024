7cf3e8512bd13f1ff1053275bbd509e721633167
mkan88 <mkan88@mountainhobbit>
2024-12-04 13:23:20 +0800

    Complete day 02 part 01

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,9 +1,34 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require
+   [clojure.java.io :refer [resource]]
+   [clojure.string :as str]
+   [advent-of-code.utils :as utils]))
+
+(defn load-reports [input]
+  (->> input
+       (str/split-lines)
+       (map utils/parse-out-longs)))
+
+(def ^:private reports (load-reports (slurp (resource "day-02.txt"))))
+
+(defn- adjacent-differ-within-range? [report low high]
+  (->> (partition 2 1 report)
+       (map #(abs (- (first %) (last %))))
+       (every? #(and (>= % low) (<= % high)))))
+
+(defn- is-strictly-monotonic? [report]
+  (or (apply > report)
+      (apply < report)))
+
+(adjacent-differ-within-range? '(1 2 3 3) 1 3)
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (->> (load-reports input)
+       (filter is-strictly-monotonic?)
+       (filter #(adjacent-differ-within-range? % 1 3))
+       count))
 
 (defn part-2
   "Day 02 Part 2"
