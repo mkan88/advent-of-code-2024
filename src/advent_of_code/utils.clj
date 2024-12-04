164b36fceb280beeb74aba4e7e21e23009c808f4
mkan88 <mkan88@mountainhobbit>
2024-12-04 18:41:53 +0800

    Add partition-at function to utils

--- src/advent_of_code/utils.clj
+++ src/advent_of_code/utils.clj
@@ -6,3 +6,13 @@
   "Parse out all numbers in `line` that are integers (longs)"
   [line]
   (map parse-long (re-seq #"[-+]?\d+" line)))
+
+(defn partition-at [pred coll]
+  (loop [c coll
+         curr-c []
+         res []]
+    (if (empty? c)
+      (conj res curr-c)
+      (if (pred (first c))
+        (recur (rest c) [(first c)] (conj res curr-c))
+        (recur (rest c) (conj curr-c (first c)) res)))))
