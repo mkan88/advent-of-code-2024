6274b926bf629fe7464d8a578c20469b90a48e0e
mkan88 <mkan88@mountainhobbit>
2024-12-04 13:42:23 +0800

    Complete day 02 part 02

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -9,7 +9,7 @@
        (str/split-lines)
        (map utils/parse-out-longs)))
 
-(def ^:private reports (load-reports (slurp (resource "day-02.txt"))))
+;; (def ^:private reports (load-reports (slurp (resource "day-02.txt"))))
 
 (defn- adjacent-differ-within-range? [report low high]
   (->> (partition 2 1 report)
@@ -20,7 +20,7 @@
   (or (apply > report)
       (apply < report)))
 
-(adjacent-differ-within-range? '(1 2 3 3) 1 3)
+;; (adjacent-differ-within-range? '(1 2 3 3) 1 3)
 
 (defn part-1
   "Day 02 Part 1"
@@ -30,7 +30,23 @@
        (filter #(adjacent-differ-within-range? % 1 3))
        count))
 
+(defn- drop-nth [n col]
+  (concat (take n col) (drop (inc n) col)))
+
+(defn- dropped-versions [report]
+  (map #(drop-nth % report) (range (count report))))
+
+(defn- any-viable? [reports]
+  (some #(and (is-strictly-monotonic? %) (adjacent-differ-within-range? % 1 3)) reports))
+
+;; (map #(drop-nth % (first reports)) (range (count (first reports))))
+;; (dropped-versions (first reports))
+;; (any-viable? (dropped-versions (first reports)))
+
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (->> (load-reports input)
+       (map dropped-versions)
+       (filter any-viable?)
+       count))
