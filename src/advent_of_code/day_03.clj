969ffbd8f09ee07527a6f70d827742aa0d0889ac
mkan88 <mkan88@mountainhobbit>
2024-12-04 18:41:19 +0800

    Complete day 03 part 02

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -28,7 +28,31 @@
        (map mul-re-seq-res)
        (reduce +)))
 
+(def ^:private mul-re2 #"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)")
+
+(defn- is-do-don't? [[expr g1 g2]]
+  (or (= expr "do()") (= expr "don't()")))
+
+(defn- partition-at [pred coll]
+  (loop [c coll
+         curr-c []
+         res []]
+    (if (empty? c)
+      (conj res curr-c)
+      (if (pred (first c))
+        (recur (rest c) [(first c)] (conj res curr-c))
+        (recur (rest c) (conj curr-c (first c)) res)))))
+
 (defn part-2
   "Day 03 Part 2"
   [input]
-  input)
+  (->> (load-mul-strs input)
+       (map #(re-seq mul-re2 %))
+       (apply concat)
+       (partition-at is-do-don't?)
+       (filter #(not= (first (first %)) "don't()"))
+       (apply concat)
+       (filter #(not= (first %) "do()"))
+       (map mul-re-seq-res)
+       (reduce +)))
+
