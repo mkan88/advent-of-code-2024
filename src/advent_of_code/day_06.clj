d69491c1e861cc6e93f4a6b70ea023bd7986a584
mkan88 <mkan88@mountainhobbit>
2024-12-06 19:00:20 +0800

    Complete day 06 part 02

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -10,10 +10,7 @@
        (mapv seq)
        (apply mapv vector)))
 
-(def ^:private guard-map (load-map (slurp (resource "day-06.txt"))))
-
-(first guard-map)
-(count (first guard-map))
+;; (def ^:private guard-map (load-map (slurp (resource "day-06.txt"))))
 
 (defn- initial-pos [m]
   (let [row-matches (map #(.indexOf % \^) m)
@@ -21,7 +18,7 @@
         x (.indexOf row-matches y)]
     [x y]))
 
-(initial-pos guard-map)
+;; (initial-pos guard-map)
 
 (defn- map-shape [m]
   [(count m) (count (first m))])
@@ -30,7 +27,7 @@
   (let [move-map {:n [0 -1] :e [1 0] :s [0 1] :w [-1 0]}]
     (mapv + pos (move-map d))))
 
-(move [2 4] :n)
+;; (move [2 4] :n)
 
 (def ^:private turn {:n :e, :e :s, :s :w, :w :n})
 
@@ -44,12 +41,34 @@
       (let [new-pos (move pos d)
             cell (get-in guard-map new-pos nil)]
         (if (nil? cell)
-            (count seen)
-            (if (= cell \#)
-              (recur pos (turn d) seen)
-              (recur new-pos d (conj seen new-pos))))))))
+          (count seen)
+          (if (= cell \#)
+            (recur pos (turn d) seen)
+            (recur new-pos d (conj seen new-pos))))))))
+
+(defn- has-cycle? [m starting-pos]
+  (loop [pos starting-pos
+         d :n
+         seen #{[pos d]}]
+    (let [new-pos (move pos d)
+          cell (get-in m new-pos nil)]
+      (if (nil? cell)
+        false
+        (if (= cell \#)
+          (recur pos (turn d) seen)
+          (if (contains? seen [new-pos d])
+            true
+            (recur new-pos d (conj seen [new-pos d]))))))))
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (let [guard-map (load-map input)
+        [M N] (map-shape guard-map)
+        free (for [x (range M) y (range N) :when (= \. (get-in guard-map [x y]))]
+               [x y])
+        starting-pos (initial-pos guard-map)]
+    (->> (filter (fn [obstacle]
+                   (has-cycle? (assoc-in guard-map obstacle \#) starting-pos))
+                 free)
+         count)))
